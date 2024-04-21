c8e52297797f26486174a968b2472c76659b62ed
mkh-web <mohnd.khalid2001@gmail.com>
2024-04-21 16:31:27 +0300

    fixing some e

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex items-center justify-center w-full">
 
       <div className=" flexCenter flex-col gap-4 text-[#111] bg-[#ffffff] rounded-lg w-[70%] md:h-80 h-96 text-center">
-        <h1 className=" capitalize text-3xl">hi there I'M Mohanad</h1>
+        <h1 className=" capitalize text-3xl">hi there I&apos;M Mohanad</h1>
         <h1 className=" ">a Data-Driven Web Architect, Transforming data into impactful web experiences is my way to bulid a website</h1>
         <div className='md:flexBetween flexCenter text-3xl w-[25%]  '>
           {SocialMedia.map((data) => (
