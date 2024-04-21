d43ad980c5988b63c361a544d38e210c3d274d63
mkh-web <mohnd.khalid2001@gmail.com>
2024-04-21 16:56:20 +0300

    next??

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,22 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center flex-col w-full">
 
       <div className=" flexCenter flex-col gap-4 text-[#111] bg-[#ffffff] rounded-lg w-[70%] md:h-80 h-96 text-center">
         <h1 className=" capitalize text-3xl">hi there I&apos;M Mohanad</h1>
-        <h1 className=" ">a Data-Driven Web Architect, Transforming data into impactful web experiences is my way to bulid a website</h1>
+        <h1 className=" ">a Data-Driven Web Architect that Transform data into impactful web experiences is my way to bulid a website</h1>
         <div className='md:flexBetween flexCenter text-3xl w-[25%]  '>
           {SocialMedia.map((data) => (
-            <Link className='opacity-50 hover:opacity-100 transition' href={data.link} key={data.icon}>{data.icon}</Link>
+            <Link className='opacity-50 hover:opacity-100 transition shadow-lg rounded-full p-2' href={data.link} key={data.icon}>{data.icon}</Link>
           ))}
         </div>
+      </div>
+
+      <div className='w-[70%]'>
+        
+
+
       </div>
 
           
